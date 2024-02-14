a72dadc8c77d20ac227ac648e37d8153f514c8b5
MrEnthropia <777ukv@gmail.com>
2024-02-14 10:43:40 +0300

    Рабочая версия c сохранением данных пользователя в БД PostgreSQL

--- src/main/java/ru/dvagoroda/MrEntropy/gorodaBot/service/TelegramBot.java
+++ src/main/java/ru/dvagoroda/MrEntropy/gorodaBot/service/TelegramBot.java
@@ -1,23 +1,29 @@
 package ru.dvagoroda.MrEntropy.gorodaBot.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.dvagoroda.MrEntropy.gorodaBot.config.BotConfig;
+import ru.dvagoroda.MrEntropy.gorodaBot.model.User;
+import ru.dvagoroda.MrEntropy.gorodaBot.model.UserRepository;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
-
+    @Autowired
+    UserRepository userRepository;
     final BotConfig config;
 
     final String HELP_TEXT= """
@@ -62,12 +68,34 @@ public class TelegramBot extends TelegramLongPollingBot {
 
             switch (messageText){
                 case "/start": startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                regirerUser(update.getMessage());
                 break;
                 case "/help": sendMessage(chatId,HELP_TEXT);
-
+                break;
                 default: sendMessage(chatId,sorry);
             }
         }
+    }
+
+    private void regirerUser(Message msg) {
+        if (userRepository.findById(msg.getChatId()).isEmpty()){
+
+            Long chatId = msg.getChatId();
+            var chat = msg.getChat();
+
+            User user = new User();
+
+            user.setChatId(chatId);
+            user.setFirstName(chat.getFirstName());
+            user.setLastName(chat.getLastName());
+            user.setUserName(chat.getUserName());
+            user.setRegisteredAt(new Timestamp(System.currentTimeMillis()));
+
+            userRepository.save(user);
+            log.info("user saved: " + user);
+            System.out.println("user saved: " + user);
+        }
+
     }
 
     private void startCommandReceived(long chatId, String name){
